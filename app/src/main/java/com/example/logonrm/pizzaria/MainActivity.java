bb14d957856f3a971d43f4059d0bc55bae4e3288
logonrm <logonrm@fiap.com.br>
2017-12-11 23:15:04 -0200

    Inserindo Listagem de parametros

--- app/src/main/java/com/example/logonrm/pizzaria/MainActivity.java
+++ app/src/main/java/com/example/logonrm/pizzaria/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.logonrm.pizzaria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.logonrm.pizzaria.model.Pedido;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.cliente)
-    TextView cliente;
+    @BindView(R.id.cbBacon)
+    CheckBox cbBacon;
+
+
+    @BindView(R.id.cbAtum)
+    CheckBox cbAtumm;
+
+
+    @BindView(R.id.rgTamanho)
+    RadioGroup rgTamanho;
+
+
+    @BindView(R.id.spPagamentos)
+    Spinner spPagamentos;
+
+    private String nomeUsuario;
+
+
+    Pedido pedido = new Pedido();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +46,43 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        String campo = getIntent().getStringExtra("USUARIO");
-        cliente.setText(campo);
+        nomeUsuario = getIntent().getStringExtra("USUARIO");
 
+        setListernerCheckbox(cbAtumm);
+        setListernerCheckbox(cbBacon);
+
+    }
+
+    private void setListernerCheckbox(final CheckBox checkbox) {
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    pedido.addSabor(checkbox.getText().toString());
+                } else {
+                    pedido.removerSabor(checkbox.getText().toString());
+                }
+            }
+        });
+
+    }
+
+    @OnClick(R.id.btFecharPedido)
+    public void fecharPedido() {
+
+        pedido.setCliente(nomeUsuario);
+        pedido.setTamanho(getTamanhoSelecionado());
+        pedido.setFormaPagamento(spPagamentos.getSelectedItem().toString());
+
+        Intent i = new Intent(this, ConfirmarPedidoActivity.class);
+        i.putExtra("PEDIDO", pedido);
+
+        startActivity(i);
+
+    }
+
+    public String getTamanhoSelecionado() {
+        return ((RadioButton) findViewById(rgTamanho.getCheckedRadioButtonId()))
+                .getText().toString();
     }
 }
